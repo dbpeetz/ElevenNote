7024667434793d3517763b20ac9058019435acf9
dbpeetz <d.peetz516@gmail.com>
2017-04-12 16:58:47 -0500

    Commit after fixing Git issue

---
+++ ElevenNote.Web/Controllers/NotesController.cs
@@ -0,0 +1,43 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Service;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    [Authorize]
+    public class NotesController : Controller
+    {
+        // GET: Notes
+        public ActionResult Index()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            var model = service.GetNotes();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+
+            service.CreateNote(model);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
